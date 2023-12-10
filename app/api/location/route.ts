d68b9c89d08b0846f6423c3823262fd4006e7d9b
Jason Desiderio <jason@thegoodfornothings.club>
2023-12-09 23:04:12 -0600

    Make sure VERCEL_ENV is populated in production

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -3,8 +3,30 @@ import type { NextRequest } from 'next/server'
 
 import { geolocation } from '@vercel/edge'
 
+export const runtime = 'edge'
+
 export function GET(request: NextRequest) {
+  console.log(process.env)
+  // geolocation doesn't return any data during local development
+  if (!process.env.VERCEL_ENV) {
+    return NextResponse.json(
+      {
+        city: 'Austin',
+        country: 'US',
+        flag: '🇺🇸',
+        countryRegion: 'TX',
+        region: 'cle1',
+        latitude: '30.2423',
+        longitude: '-97.7672',
+      },
+      {
+        status: 200,
+      }
+    )
+  }
+
   const location = geolocation(request)
+
   // You can also get the city using dot notation on the function
   // const city = geolocation(request).city;
   return NextResponse.json(location, {
