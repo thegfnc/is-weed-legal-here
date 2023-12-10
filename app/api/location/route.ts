721bc81879642900be3c92addeba4f5983ed781e
Jason Desiderio <jason@thegoodfornothings.club>
2023-12-09 22:52:54 -0600

    Test out more location stuff

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -3,8 +3,6 @@ import type { NextRequest } from 'next/server'
 
 import { geolocation } from '@vercel/edge'
 
-export const runtime = 'edge' // 'nodejs' is the default
-
 export function GET(request: NextRequest) {
   const location = geolocation(request)
   // You can also get the city using dot notation on the function
