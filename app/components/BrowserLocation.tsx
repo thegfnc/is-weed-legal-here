0d28878d867f0d7beb49f5717f31d50d0ae4b018
Jason Desiderio <jason@thegoodfornothings.club>
2023-12-10 23:02:02 -0600

    Update text

--- app/components/BrowserLocation.tsx
+++ app/components/BrowserLocation.tsx
@@ -13,7 +13,7 @@ type BrowserLocationProps = {
 
 enum LoadingState {
   ASKING_FOR_PERMISSION = "Don't hold out on us. Allow your location to find out if you can legally light one up!",
-  RETRIEVING_LOCATION = "We're just waiting on your browser to pass us your location.",
+  RETRIEVING_LOCATION = "We're just waiting on the browser to pass us your location.",
   SEARCHING_FOR_DATA = "Just a moment while we hit up Google Maps like we're at the bottom of the bag.",
   SUCCESS = 'Success',
 }
