ee970b511dffa927ce26ca5a433bcdc1952be15c
Jason Desiderio <jason@thegoodfornothings.club>
2024-01-07 17:27:13 -0600

    Fix type error

--- app/helpers/getResultUrlFromCurrentLocation.ts
+++ app/helpers/getResultUrlFromCurrentLocation.ts
@@ -1,15 +1,17 @@
 import { CurrentLocation } from '@/app/types'
 
+const FILTER_OUT = 'FILTER_OUT'
+
 export default function getResultUrlFromCurrentLocation(
   currentLocation: CurrentLocation
 ) {
   const urlParts = [
     '/result',
-    encodeURIComponent(currentLocation.country),
-    encodeURIComponent(currentLocation.administrativeAreaLevel1),
-    encodeURIComponent(currentLocation.administrativeAreaLevel2),
-    encodeURIComponent(currentLocation.locality),
-    encodeURIComponent(currentLocation.postalCode),
+    encodeURIComponent(currentLocation.country || '-'),
+    encodeURIComponent(currentLocation.administrativeAreaLevel1 || '-'),
+    encodeURIComponent(currentLocation.administrativeAreaLevel2 || '-'),
+    encodeURIComponent(currentLocation.locality || '-'),
+    encodeURIComponent(currentLocation.postalCode || '-'),
   ]
 
   return urlParts.join('/')
