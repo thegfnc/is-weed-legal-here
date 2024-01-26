16a9b15a5bfdad34d3666ac85a76a874ed10a1f6
Jason Desiderio <jason@thegoodfornothings.club>
2024-01-26 14:07:16 -0600

    Match the hover text color on CTA with background

--- app/components/CallToActionButton.tsx
+++ app/components/CallToActionButton.tsx
@@ -1,4 +1,9 @@
+import { useContext } from 'react'
 import { MdSearch } from 'react-icons/md'
+import {
+  BackgroundColor,
+  BackgroundColorContext,
+} from '../contexts/backgroundColorContext'
 
 type CallToActionButtonProps = {
   text: string
@@ -6,9 +11,23 @@ type CallToActionButtonProps = {
 }
 
 const CallToActionButton = ({ text, linkUrl }: CallToActionButtonProps) => {
+  const backgroundColor = useContext(BackgroundColorContext)
+
+  // for some reason, tailwind doesn't let you concatenate classes
+  // with the hover: prefix at runtime, so we have to do this
+  let hoverTextColor = null
+
+  if (backgroundColor === BackgroundColor.GREEN) {
+    hoverTextColor = 'hover:text-brand-green'
+  } else if (backgroundColor === BackgroundColor.RED) {
+    hoverTextColor = 'hover:text-brand-red'
+  } else {
+    hoverTextColor = 'hover:text-brand-yellow'
+  }
+
   return (
     <a
-      className='flex items-center rounded-full border-2 border-brand-purple py-2 pl-4 pr-5 text-[16px] transition-colors hover:bg-brand-purple hover:text-brand-yellow md:text-[18px]'
+      className={`flex items-center rounded-full border-2 border-brand-purple py-2 pl-4 pr-5 text-[16px] transition-colors hover:bg-brand-purple ${hoverTextColor} md:text-[18px]`}
       href={linkUrl}
       target='_blank'
     >
