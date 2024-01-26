80afcb01b0e1077c6633ca02c2e03522e4d20e3e
Jason Desiderio <jason@thegoodfornothings.club>
2024-01-26 10:26:56 -0600

    Disable sentry on local dev

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,7 +4,11 @@
 
 import * as Sentry from '@sentry/nextjs'
 
+console.log('NEXT_PUBLIC_VERCEL_ENV', process.env.NEXT_PUBLIC_VERCEL_ENV)
+
 Sentry.init({
+  enabled: process.env.NEXT_PUBLIC_VERCEL_ENV !== 'development',
+
   dsn: 'https://b33f06fd16bbb495c85d256ee5f32729@o4506369040187392.ingest.sentry.io/4506369042415616',
 
   // Adjust this value in production, or use tracesSampler for greater control
