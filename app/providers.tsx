8c97f935e43c68f08702b24a1e3385baebc2ef74
Jason Desiderio <jason@thegoodfornothings.club>
2024-03-08 13:46:00 -0600

    Add experimental streaming

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,16 +4,16 @@
 import { ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-// import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
 
 const queryClient = new QueryClient()
 
 export function Providers(props: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ReactQueryStreamedHydration> */}
-      {props.children}
-      {/* </ReactQueryStreamedHydration> */}
+      <ReactQueryStreamedHydration>
+        {props.children}
+      </ReactQueryStreamedHydration>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
