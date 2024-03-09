a61b1726279569ec57066c853ccc115b09245676
Jason Desiderio <jason@thegoodfornothings.club>
2024-03-08 23:20:23 -0600

    Add console log

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -29,6 +29,7 @@ export async function POST(req: NextRequest) {
     // If the `_type` is `page`, then all `client.fetch` calls with
     // `{next: {tags: ['page']}}` will be revalidated
     revalidateTag(body._type)
+    console.log('Revalidated: ', body._type)
 
     return NextResponse.json({ body })
   } catch (err: any) {
