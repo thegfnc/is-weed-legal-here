96bcb6ee6ddb2011afc3fbad393fc0d8a7ef9a37
Jason Desiderio <jason@thegoodfornothings.club>
2024-03-10 14:37:10 -0500

    Some mobile touch ups

--- app/components/Overview.tsx
+++ app/components/Overview.tsx
@@ -6,8 +6,10 @@ type OverviewProps = { body: TypedObject[] }
 export default function Overview({ body }: OverviewProps) {
   return (
     <div>
-      <h2 className='text-[32px] font-bold leading-normal'>Overview</h2>
-      <div className='portable-text mt-6 text-[20px] leading-tight'>
+      <h2 className='text-3xl font-bold leading-normal md:text-[32px]'>
+        Overview
+      </h2>
+      <div className='portable-text mt-6 text-base leading-tight md:text-[20px]'>
         <PortableText value={body} />
       </div>
     </div>
