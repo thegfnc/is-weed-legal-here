a3a73314944ea3563379b93b6a5dd446b8b7c5ab
Jason Desiderio <jason@thegoodfornothings.club>
2024-03-31 16:58:24 -0500

    Consolidate layout into page now that use client is removed

--- app/browse/[[...location]]/page.tsx
+++ app/browse/[[...location]]/page.tsx
@@ -5,12 +5,26 @@ import getLegalityDataForLocation from '@/app/helpers/getLegalityDataForLocation
 import { sanityFetch } from '@/app/data/client'
 import BrowseLocation from './BrowseLocation'
 import { CMSCountry } from '@/app/types'
+import { Metadata, ResolvingMetadata } from 'next'
 
 type BrowsePageProps = {
   params: {
     location: string[]
   }
 }
+
+type GenerateMetadataParams = {
+  params: {
+    location: string[]
+  }
+}
+
+const LOCATION_COUNT_QUERY = `
+  count(*[_type == 'IIHD_country' ||
+    _type == 'IIHD_administrativeAreaLevel1' ||
+    _type == 'IIHD_administrativeAreaLevel2' ||
+    _type == 'IIHD_locality'])
+`
 
 const ALL_COUNTRIES_QUERY = `
   *[_type == 'IIHD_country'] | order(name) {
@@ -45,12 +59,55 @@ const COUNTRY_MATCH_QUERY = `
   }
 `
 
-const LOCATION_COUNT_QUERY = `
-  count(*[_type == 'IIHD_country' ||
-    _type == 'IIHD_administrativeAreaLevel1' ||
-    _type == 'IIHD_administrativeAreaLevel2' ||
-    _type == 'IIHD_locality'])
-`
+export async function generateMetadata(
+  { params: { location } }: GenerateMetadataParams,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { openGraph } = await parent
+  const pathname = '/browse'
+
+  const metadata: Metadata = {
+    alternates: {
+      canonical: pathname,
+    },
+    openGraph: {
+      ...openGraph,
+      url: pathname,
+    },
+  }
+
+  if (location) {
+    const currentLocation = getCurrentLocationFromUrlParams(location)
+    const data = await sanityFetch<CMSCountry[]>({
+      query: COUNTRY_MATCH_QUERY,
+      params: { country: currentLocation.country },
+      tags: [
+        'IIHD_country',
+        'IIHD_administrativeAreaLevel1',
+        'IIHD_administrativeAreaLevel2',
+        'IIHD_locality',
+      ],
+    })
+
+    const legalityData = getLegalityDataForLocation(currentLocation, data)
+
+    const closestLocationName =
+      legalityData?.closestMatchKey &&
+      currentLocation[legalityData.closestMatchKey]
+
+    metadata.title = `Is weed legal in ${closestLocationName || 'your area'}?`
+
+    if (metadata.alternates) {
+      metadata.alternates.canonical += '/' + location.join('/')
+    }
+
+    if (metadata.openGraph) {
+      metadata.openGraph.url += '/' + location.join('/')
+    }
+  }
+
+  return metadata
+}
 
 export default async function BrowsePage({
   params: { location = [] },
