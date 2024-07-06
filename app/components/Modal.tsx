87d6f6c0b63cb620a2bd8e109f3630e8e4d8a8b5
Jason Desiderio <jason@thegoodfornothings.club>
2024-07-06 16:13:20 -0500

    Fix disclaimer modal layout

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,6 @@
 import { MdClose } from 'react-icons/md'
 import modalContent from '@/app/data/modals'
+import { useEffect } from 'react'
 
 export enum ModalType {
   DISCLAIMER = 'DISCLAIMER',
@@ -11,15 +12,28 @@ type ModalProps = {
 }
 
 export default function Modal({ type, onClose }: ModalProps) {
+  useEffect(() => {
+    if (type) {
+      document.body.style.overflow = 'hidden'
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [type])
+
   if (!type) return null
 
   return (
-    <div className='absolute left-0 top-0 h-screen w-screen text-left text-lg font-medium'>
+    <div className='fixed left-0 top-0 h-screen w-screen text-left text-lg font-medium'>
       <div
         className='absolute left-0 top-0 h-screen w-screen bg-black opacity-40'
         onClick={onClose}
       ></div>
-      <div className='pointer-events-none absolute left-0 top-0 h-screen w-screen overflow-y-scroll'>
+      <div
+        className='pointer-events-none absolute left-0 top-0 h-screen w-screen overflow-y-scroll'
+        onScroll={e => e.stopPropagation()}
+      >
         <div className='pointer-events-none relative flex w-screen justify-center'>
           <div className='pointer-events-auto m-4 w-full max-w-xl border-2 border-brand-purple bg-white'>
             <div className='flex items-center justify-between border-b-2 border-brand-purple px-7 py-5'>
